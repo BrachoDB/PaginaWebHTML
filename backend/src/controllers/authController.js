fc8fecf0d3246b1760504b255a6a49943120ef0f
Angelina Ochoa <angelinaochoa549@gmail.com>
2026-02-10 17:01:29 -0500

    fix(controllers): corrige el campo email parael login del sitio

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
     try {
-        const { username, password, roleId } = req.body;
+        const { username, password, roleId, email } = req.body;
         // Check if user exists
-        const existingUser = await User.findOne({ where: { username } });
+        const existingUser = await User.findOne({ where: { email } });
         if (existingUser) return res.status(400).json({ message: 'User already exists' });
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({ nombre: username, username, password: hashedPassword, rol_id: roleId });
+        const user = await User.create({ nombre:email.split('@')[0] , username, password: hashedPassword, rol_id: roleId });
         res.status(201).json({ message: 'User created successfully', userId: user.id });
     } catch (error) {
         res.status(500).json({ message: 'Error registering user', error: error.message });
@@ -19,17 +19,17 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ where: { username }, include: Role });
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email }, include: Role });
         if (!user) return res.status(401).json({ message: 'Invalid credentials' });
 
         if (!user.isActive) return res.status(403).json({ message: 'User is inactive' });
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+        if (!isMatch) return res.status(401).json({ message: 'Invalid credentials/password' });
 
         const token = jwt.sign(
-            { id: user.id, username: user.username, role: user.Role.nombre },
+            { id: user.id, email: user.email, role: user.Role.nombre },
             process.env.JWT_SECRET,
             { expiresIn: '24h' }
         );
