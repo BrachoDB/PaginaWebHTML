b23a407eb6a8ebd616a3c4ba664c4436d1f49fdb
sebastian <sebascova18qgmail.com>
2026-02-20 15:11:42 -0500

    feat(api): añade  logica para levantar api serverless

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,14 @@
 const app = require('../src/app');
+const sequelize = require("..src/config/db")
+
+(async () => {
+    try{
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch(error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
 // Note: In a real serverless env, you might need to handle DB connection re-use explicitly or use a connection fool.
 // For Vercel, this exports the Express app as a serverless function.
 module.exports = app;
