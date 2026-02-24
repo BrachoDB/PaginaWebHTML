149dc33bc16d0dd3db038602c4f0a66c9a175cdd
sebastian <sebascova18qgmail.com>
2026-02-24 14:04:06 -0500

    fix(config): añade puerto para la conexion de base de datos

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   logging: false,
